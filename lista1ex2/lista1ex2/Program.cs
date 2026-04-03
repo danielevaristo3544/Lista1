d6d7987fc5105b9a59e627756eab6eeb066bd1de
danielevaristo3544 <daniel.evaristo@aluno.ifsp.edu.br>
2026-04-02 22:34:23 -0300

    Add files via upload

---
+++ lista1ex2/lista1ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int a;
+
+            Console.WriteLine("Informe a aresta do quadrado:");
+            l = int.Parse(Console.ReadLine());
+
+            a = l * l;
+
+            Console.WriteLine("O resultado da área é:");
+            Console.WriteLine(a);
+        }
+    }
+}
